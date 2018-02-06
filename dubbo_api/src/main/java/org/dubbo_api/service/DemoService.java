826d75ad73568efd39c5b70a8d0178214d9a71ec
Silenburst <renranzhike@126.com>
2018-02-06 14:05:54 +0800

    first commit

---
+++ dubbo_api/src/main/java/org/dubbo_api/service/DemoService.java
@@ -0,0 +1,6 @@
+package org.dubbo_api.service;
+
+public interface DemoService {
+
+	public String sayHello(String name);
+}
---
+++ dubbo_provider/src/main/java/org/dubbo_provider/service/DemoServiceImpl.java
@@ -0,0 +1,11 @@
+package org.dubbo_provider.service;
+
+import org.dubbo_api.service.DemoService;
+
+public class DemoServiceImpl implements DemoService {
+	public String sayHello(String name) {
+		// TODO Auto-generated method stub
+		return name;
+	}
+
+}
